01cf28e0d4ab798c3feb70a0364e97dfa5e6d652
outsourceBPM15 <outsourceBPM15@msb.com.vn>
2023-02-13 11:40:21 +0700

    second commit

--- src/app/api.factory.ts
+++ src/app/api.factory.ts
@@ -1,9 +1,11 @@
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 import { DOMAIN, HTTP_METHOD } from "./constants";
-import { Injectable } from "@angular/core";
 import { DATA_TYPE } from "./constants";
+
+const { GET, POST, PUT, DELETE } = HTTP_METHOD
 
 @Injectable({
   providedIn: 'root'
@@ -14,16 +16,17 @@ export class ApiFactory {
   constructor(private http: HttpClient) {
   }
 
-  public fetchApi(data: any, method: string, typeResult: string = DATA_TYPE.ANY, param: string = ''): Observable<any> {
-    const callApi = this.http
+  public fetchApi(data: any, method: string = GET, typeResult: string = DATA_TYPE.ANY, param: string = ''): Observable<any> {
+    const callApi = this.http;
+
     switch (method) {
-      case HTTP_METHOD.GET:
+      case GET:
         return callApi.get<typeof typeResult>(`${DOMAIN}/${param}`)
-      case HTTP_METHOD.PUT:
+      case PUT:
         return callApi.put<typeof typeResult>(`${DOMAIN}/${param}/${data.id}`, data)
-      case HTTP_METHOD.POST:
+      case POST:
         return callApi.post<typeof typeResult>(`${DOMAIN}/${param}`, data)
-      case HTTP_METHOD.DELETE:
+      case DELETE:
         return callApi.delete<typeof typeResult>(`${DOMAIN}/${param}/${data}`)
       default:
         throw new Error('Can not call api, the method type is invalid')
--- src/app/services/xyz.service.ts
+++ src/app/services/xyz.service.ts
@@ -6,8 +6,6 @@ import { ApiFactory } from "../api.factory";
 import { DATA_TYPE } from "../constants";
 import { INTERFACE_INFO } from "../app.interface";
 
-const {GET, POST, PUT, DELETE} = HTTP_METHOD
-
 @Injectable({
   providedIn: 'root'
 })
@@ -18,18 +16,18 @@ export class XyzService {
   }
 
   public getData(): Observable<any> {
-    return this.apiFactory.fetchApi(null, GET, DATA_TYPE.ARRAY, 'data')
+    return this.apiFactory.fetchApi(null, HTTP_METHOD.GET, DATA_TYPE.ARRAY, 'data')
   }
 
   public addData(data: object): Observable<any> {
-    return this.apiFactory.fetchApi(data, POST, DATA_TYPE.OBJECT, 'data')
+    return this.apiFactory.fetchApi(data, HTTP_METHOD.POST, DATA_TYPE.OBJECT, 'data')
   }
 
   public updateData(data: INTERFACE_INFO): Observable<any> {
-    return this.apiFactory.fetchApi(data, PUT, DATA_TYPE.OBJECT, 'data')
+    return this.apiFactory.fetchApi(data, HTTP_METHOD.PUT, DATA_TYPE.OBJECT, 'data')
   }
 
   public deleteData(id: string): Observable<any> {
-    return this.apiFactory.fetchApi(id, DELETE, DATA_TYPE.OBJECT, 'data')
+    return this.apiFactory.fetchApi(id, HTTP_METHOD.DELETE, DATA_TYPE.OBJECT, 'data')
   }
 }
